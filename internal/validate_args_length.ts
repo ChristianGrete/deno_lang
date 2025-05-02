03d8f03d24e9c86378fb71acca643ff1b7dbed2d
Christian Grete <webmaster@christiangrete.com>
2025-05-02 17:43:18 +0200

    feat(internal): add validate_args_length module

---
+++ internal/validate_args_length.ts
@@ -0,0 +1,14 @@
+export const validateArgsLength = (
+  args: unknown,
+  expected: number = 1,
+): void => {
+  const { length } = args as unknown[];
+
+  if (length !== expected) {
+    throw new TypeError(
+      `Invalid number of arguments: expected ${expected}, got ${length}`,
+    );
+  }
+};
+
+export default validateArgsLength;
