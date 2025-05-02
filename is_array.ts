593fcc662050a3c8ca278b785a783e07686fc954
Christian Grete <webmaster@christiangrete.com>
2025-05-02 21:59:44 +0200

    feat: add is_array module
    - includes is_array test
    - also update main module

---
+++ is_array.ts
@@ -0,0 +1,34 @@
+/**
+ * Utility module for checking whether a value is an array.
+ *
+ * @author Miller Medeiros <miller@millermedeiros.com>
+ * @author Christian Grete <webmaster@christiangrete.com>
+ * @author ChatGPT <chatgpt@openai.com>
+ * @copyright © 2025 Christian Grete
+ * @license MIT
+ * @module lang/is_array
+ * @see {@link https://github.com/mout/mout/blob/v1.2.4/src/lang/isArray.js|mout@1.2.4/lang/isArray}
+ */
+
+import { unsetPrototype } from "./internal/unset_prototype.ts";
+import { validateArgsLength } from "./internal/validate_args_length.ts";
+
+const { isArray: nativeIsArray } = Array;
+
+/**
+ * Checks whether a value is an array.
+ *
+ * @function
+ * @name lang/is_array.isArray
+ * @param {unknown} value – The value to check.
+ * @returns {value is unknown[]} Whether the value is an array.
+ */
+export function isArray(
+  value: unknown,
+): value is unknown[] {
+  validateArgsLength(arguments);
+
+  return nativeIsArray(value);
+}
+
+unsetPrototype(isArray);
---
+++ is_array_test.ts
@@ -0,0 +1,39 @@
+import { assert, assertFalse, assertThrows } from "@std/assert";
+import { isArray } from "./is_array.ts";
+
+Deno.test("isArray() returns true for real arrays", () => {
+  assert(isArray([]));
+  // deno-lint-ignore no-array-constructor
+  assert(isArray(new Array()));
+  assert(isArray([1, 2, 3]));
+});
+
+Deno.test("isArray() returns false for non-array types", () => {
+  assertFalse(isArray(undefined));
+  assertFalse(isArray(null));
+  assertFalse(isArray(true));
+  assertFalse(isArray(42));
+  assertFalse(isArray("hello"));
+  assertFalse(isArray(Symbol("sym")));
+  assertFalse(isArray(10n));
+  assertFalse(isArray(() => {}));
+  assertFalse(isArray({}));
+  assertFalse(isArray(Object.create(null)));
+});
+
+Deno.test("isArray() returns false for array-like but non-array objects", function () {
+  assertFalse(isArray(arguments));
+  assertFalse(isArray({ length: 1 }));
+  assertFalse(isArray(new Set()));
+  assertFalse(isArray(new Map()));
+  assertFalse(isArray(new Uint8Array(3)));
+  // assertFalse(isArray(document?.querySelectorAll?.("div") ?? {}));
+});
+
+Deno.test("isArray() throws on invalid number of arguments", () => {
+  // @ts-expect-error – test missing argument
+  assertThrows(() => isArray(), TypeError);
+
+  // @ts-expect-error – test too many arguments
+  assertThrows(() => isArray([], []), TypeError);
+});
