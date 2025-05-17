1715f9df3a9ce42e6381fc254143688ef45ffd23
Christian Grete <webmaster@christiangrete.com>
2025-05-17 23:10:59 +0200

    chore: utilize eslint

--- commitlint.ts
+++ commitlint.ts
@@ -2,4 +2,5 @@ import type { UserConfig } from "npm:@commitlint/types";
 
 const config: UserConfig = { extends: ["@commitlint/config-conventional"] };
 
+// eslint-disable-next-line import/no-default-export
 export default config;
--- eslint.ts
+++ eslint.ts
@@ -1,10 +1,32 @@
 import tsParser from "npm:@typescript-eslint/parser";
-import type { Linter } from "npm:eslint";
+import type { ESLint, Linter } from "npm:eslint";
+import * as importPluginNs from "npm:eslint-plugin-import";
+import jsoncPlugin from "npm:eslint-plugin-jsonc";
+import simpleImportSortPlugin from "npm:eslint-plugin-simple-import-sort";
+import jsoncParser from "npm:jsonc-eslint-parser";
+
+const importPlugin = importPluginNs.default ?? importPluginNs;
 
 const config: Linter.Config[] = [{
   files: ["**/*.ts"],
   languageOptions: { parser: tsParser, parserOptions: { ecmaVersion: "latest", sourceType: "module" } },
-  rules: {}, // Keine Regeln = keine Eingriffe
+  plugins: { "import": importPlugin, "simple-import-sort": simpleImportSortPlugin },
+  rules: {
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-default-export": "error",
+    "import/no-duplicates": "error",
+    "import/no-named-default": "error",
+    "import/no-useless-path-segments": "error",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+  },
+}, {
+  files: ["**/*.json"],
+  languageOptions: { parser: jsoncParser },
+  plugins: { jsonc: jsoncPlugin as unknown as ESLint.Plugin },
+  rules: { "jsonc/sort-keys": "error" },
 }];
 
+// eslint-disable-next-line import/no-default-export
 export default config;
