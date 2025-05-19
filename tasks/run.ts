d6119c2c516e2c5d074a557d4ef7e20104e8aff4
Christian Grete <webmaster@christiangrete.com>
2025-05-19 17:17:40 +0200

    chore: create run-s and run-p tasks

--- tasks/run.ts
+++ tasks/run.ts
@@ -1,6 +1,5 @@
 export const run = async (args: string[]) => {
   const cmd = new Deno.Command(args[0], { args: args.slice(1), stderr: "inherit", stdout: "inherit" });
-
   const { code } = await cmd.output();
 
   if (code !== 0) {
@@ -10,4 +9,28 @@ export const run = async (args: string[]) => {
   }
 };
 
-if (import.meta.main) await run(Deno.args);
+if (import.meta.main) {
+  const args = [...Deno.args];
+  const mode = args.shift();
+  const isParallel = mode === "-p";
+  const isSerial = mode === "-s";
+
+  if (!isSerial && !isParallel) {
+    console.error("Usage: deno task run -p|-s <tasks...>");
+
+    Deno.exit(1);
+  }
+
+  const runTask = (name: string) => run(["deno", "task", name]);
+
+  if (isSerial) {
+    for (const task of args) {
+      await runTask(task);
+    }
+  } else {
+    const results = await Promise.allSettled(args.map(runTask));
+    const hasFailure = results.some((r) => r.status === "rejected");
+
+    if (hasFailure) Deno.exit(1);
+  }
+}
