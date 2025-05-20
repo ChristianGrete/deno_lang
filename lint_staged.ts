627046d0b8e25d0df6ef59c7ddfcc1906b3b74d1
Christian Grete <webmaster@christiangrete.com>
2025-05-20 22:02:01 +0200

    refactor: do not make check_filenames.ts a task

--- lint_staged.ts
+++ lint_staged.ts
@@ -1,7 +1,7 @@
 import type { Configuration } from "lint-staged";
 
 const config: Configuration = {
-  "*": "deno task check-filenames",
+  "*": "deno run -A ./git_hooks/check_filenames.ts",
   "*.{md,yml}": "deno task dprint-fmt",
   "*.json": ["deno task eslint-fix", "deno task dprint-fmt"],
   "*.ts": ["deno check", "deno lint --fix", "deno task eslint-fix", "deno task dprint-fmt", "deno task test"],
