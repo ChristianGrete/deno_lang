eea48a93c7a71f5e939136aab978f2e921de6702
Christian Grete <webmaster@christiangrete.com>
2025-06-04 20:23:36 +0200

    fix: use proper error message

--- internal/validate_plain_obj_arg.ts
+++ internal/validate_plain_obj_arg.ts
@@ -3,6 +3,6 @@ import { getType } from "../type_of.ts";
 
 export const validatePlainObjArg = (name: string, value: unknown): void => {
   if (!isObjectLiteral(value)) {
-    throw new TypeError(`Invalid argument '${name}': expected string, got ${getType(value)}`);
+    throw new TypeError(`Invalid argument '${name}': expected plain object, got ${getType(value)}`);
   }
 };
