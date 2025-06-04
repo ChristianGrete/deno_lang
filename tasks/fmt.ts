2c2d8ece549c2a79ffdfe4c40afec854a4c6d3d7
Christian Grete <webmaster@christiangrete.com>
2025-06-04 11:38:17 +0200

    chore: fix task crashes on invalid file extensions

--- tasks/fmt.ts
+++ tasks/fmt.ts
@@ -6,15 +6,18 @@
 
 import { bold, red } from "@std/fmt/colors";
 
+import { filterFilesForLinters } from "./lint.ts";
 import { run } from "./run.ts";
 
 if (import.meta.main) {
-  const files = Deno.args;
+  const { dprintFiles, eslintFiles, hasNoFiles, lintFiles } = filterFilesForLinters(Deno.args);
 
   try {
-    await run(["deno", "lint", "-q", "--fix", ...files]);
-    await run(["deno", "task", "eslint-fix", "--quiet", ...files]);
-    await run(["deno", "task", "dprint-fmt", "-L=warn", ...files]);
+    if (hasNoFiles || lintFiles.length > 0) await run(["deno", "lint", "-q", "--fix", ...lintFiles]);
+
+    if (hasNoFiles || eslintFiles.length > 0) await run(["deno", "task", "eslint-fix", "--quiet", ...eslintFiles]);
+
+    if (hasNoFiles || dprintFiles.length > 0) await run(["deno", "task", "dprint-fmt", "-L=warn", ...dprintFiles]);
   } catch (err: unknown) {
     if (err instanceof Error) {
       console.error(`${bold(red("Error"))} ${red("Failed to run task 'fmt':")}`);
--- tasks/lint.ts
+++ tasks/lint.ts
@@ -5,16 +5,49 @@
  */
 
 import { bold, dim, red } from "@std/fmt/colors";
+import { extname } from "@std/path";
 
 import { run } from "./run.ts";
 
+export const filterFilesForLinters = (allFiles: string[]) => {
+  const dprintFiles: string[] = [];
+  const eslintFiles: string[] = [];
+  const hasNoFiles = allFiles.length === 0;
+  const ignoredFiles: string[] = [];
+  const lintFiles: string[] = [];
+
+  if (!hasNoFiles) {
+    for (const file of allFiles) {
+      switch (extname(file).toLowerCase()) {
+        case ".ts":
+          lintFiles.push(file);
+          // fallthrough into ".json" case
+        case ".json":
+          eslintFiles.push(file);
+          dprintFiles.push(file);
+          break;
+        case ".md":
+        case ".yml":
+          dprintFiles.push(file);
+          break;
+        default:
+          ignoredFiles.push(file);
+      }
+    }
+  }
+
+  return { allFiles, dprintFiles, eslintFiles, hasNoFiles, ignoredFiles, lintFiles };
+};
+
 if (import.meta.main) {
-  const files = Deno.args;
+  const { dprintFiles, eslintFiles, hasNoFiles, lintFiles } = filterFilesForLinters(Deno.args);
 
   try {
-    await run(["deno", "lint", "-q", ...files]);
-    await run(["deno", "task", "eslint", "--quiet", ...files]);
-    await run(["deno", "task", "dprint-check", "-L=warn", ...files]);
+    if (hasNoFiles || lintFiles.length > 0) await run(["deno", "lint", "-q", ...lintFiles]);
+
+    if (hasNoFiles || eslintFiles.length > 0) await run(["deno", "task", "eslint", "--quiet", ...eslintFiles]);
+
+    if (hasNoFiles || dprintFiles.length > 0) await run(["deno", "task", "dprint-check", "-L=warn", ...dprintFiles]);
   } catch (err: unknown) {
     if (err instanceof Error) {
       console.error(`${bold(red("Error"))} ${dim("Failed to run task 'lint':")}`);
